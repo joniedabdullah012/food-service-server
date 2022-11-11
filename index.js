0c003b2d84c3d8ed557786b9a41c153be3125203
joniedabdullah012 <jaarafathbd360@gmail.com>
2022-11-12 02:32:33 +0600

    services data add

--- index.js
+++ index.js
@@ -23,11 +23,58 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+// homeservice
+
+
+
+// ******services
+
+async function run() {
+    try {
+        const serviceCollection = client.db('foodService').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+
+            const services = await cursor.toArray();
+
+            res.send(services)
+        })
+
+
+        const serviceCollections = client.db('foodService').collection('services')
+
+        app.get('/homeservices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollections.find(query).limit(3)
+
+            const services = await cursor.toArray();
+
+
+            res.send(services)
+        })
+
+
+
+
+
+    }
+
+    finally {
+
+    }
+
+
+}
+
+run().catch(err => console.error(err));
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
